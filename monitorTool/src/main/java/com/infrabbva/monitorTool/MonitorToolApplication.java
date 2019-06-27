e88d53b64f4e6e58cbdf0f013f7c2acefea80b8f
CodeWarrior70 <ismael.cristobal.irra@gmail.com>
2019-06-27 15:29:25 -0500

    Proyectil_Base
    Base de Spring Boot

---
+++ monitorTool/src/main/java/com/infrabbva/monitorTool/MonitorToolApplication.java
@@ -0,0 +1,13 @@
+package com.infrabbva.monitorTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorToolApplication.class, args);
+	}
+
+}
